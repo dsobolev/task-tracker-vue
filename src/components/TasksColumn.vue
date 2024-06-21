a6e87227666c453474d2e743a1f5c956ed0ecf60
dsobolev <denis.sobolev1@gmail.com>
2024-06-21 11:51:21 +0100

    Tasks column component

---
+++ src/components/TasksColumn.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { type TaskEntity } from '../common/interfaces'
+import Task from './Task.vue'
+
+defineProps<{
+    title: string,
+    tasks: TaskEntity[]
+}>()
+</script>
+
+<template>
+    <h3>{{ title }}</h3>
+    <Task v-for="task in tasks"
+          :data="task"
+          :key="task.id" />
+</template>
+
+<style scoped>
+
+</style>
