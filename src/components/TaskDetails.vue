3390bf4dddfb7e4731b350adfb20ba0f95e220fd
dsobolev <denis.sobolev1@gmail.com>
2024-06-27 14:28:51 +0100

    TaskDetails component

---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { type TaskEntity, TaskStatus } from '@/common/interfaces';
+const props = defineProps<{
+    task: TaskEntity,
+}>()
+
+const title = computed(() => `#${props.task.id} ${props.task.title}`)
+const statusText = computed(() => {
+    let text = ''
+    switch (props.task.status) {
+        case TaskStatus.ToDo:
+            text = 'To Do'
+            break;
+        case TaskStatus.InProgress:
+            text = 'In Progress'
+            break;
+        case TaskStatus.Done:
+            text = 'Done'
+            break;
+    }
+
+    return text
+})
+</script>
+
+<template>
+    <div>
+        <h3>{{ title }}</h3>
+        <span>{{ statusText }}</span>
+        <p>{{ props.task.description }}</p>
+    </div>
+</template>
---
+++ src/components/__tests__/TaskDetails.spec.ts
@@ -0,0 +1,39 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import TaskDetails from '@/components/TaskDetails.vue'
+import { TaskStatus } from '@/common/interfaces'
+
+const task = {
+    "id": 1,
+    "title": "Title 1",
+    "status": 0,
+    "description": "Lorem ipsum"
+}
+
+describe('TaskDetails', () => {
+    it('renders all task data', () => {
+        const wrapper = mount(TaskDetails, { props: { task } })
+        expect(wrapper.text()).toContain(task.id)
+        expect(wrapper.text()).toContain(task.title)
+        expect(wrapper.text()).toContain(task.description)
+    })
+
+    it('displays ToDo status', () => {
+        const wrapper = mount(TaskDetails, { props: { task } })
+        expect(wrapper.text()).toContain('To Do')  
+    })
+
+    it('displays In Progress status', () => {
+        task.status = TaskStatus.InProgress
+        const wrapper = mount(TaskDetails, { props: { task } })
+        expect(wrapper.text()).toContain('In Progress')  
+    })
+    
+    it('displays Done status', () => {
+        task.status = TaskStatus.Done
+        const wrapper = mount(TaskDetails, { props: { task } })
+        expect(wrapper.text()).toContain('Done')  
+    })
+
+})
