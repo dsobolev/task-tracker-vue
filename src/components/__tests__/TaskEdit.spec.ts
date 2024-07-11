fb263bea4ec95284617491803cd8deeeada52ef0
dsobolev <denis.sobolev1@gmail.com>
2024-07-11 12:17:23 +0100

    Initial tests for TaskEdit

---
+++ src/components/__tests__/TaskEdit.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import TaskEdit from '@components/TaskEdit.vue'
+
+describe('TaskEdit', () => {
+    describe('when no task provided:', () => {
+        it.todo('shows labels for all fields')
+        it.todo('shows empty fields')
+        it.todo('shows default ToDo status')
+    })
+})
