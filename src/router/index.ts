5106c2e6e0c45da8783a3d4e0af195da54fa0851
dsobolev <denis.sobolev1@gmail.com>
2024-07-15 22:15:24 +0100

    Fmt

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,19 +3,19 @@ import AllTasksView from '../views/AllTasksView.vue'
 import TaskEditView from '../views/TaskEditView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'tasksGrid',
-      component: AllTasksView,
-    },
-    {
-      path: '/task/:taskId',
-      name: 'taskEdit',
-      component: TaskEditView,
-    },
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'tasksGrid',
+            component: AllTasksView,
+        },
+        {
+            path: '/task/:taskId',
+            name: 'taskEdit',
+            component: TaskEditView,
+        },
+    ]
 })
 
 export default router
