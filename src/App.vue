67661a99c9d1cd0b8026c42f5b695a1d436abe2b
dsobolev <denis.sobolev1@gmail.com>
2024-07-30 20:02:53 +0100

    Formatting

--- src/App.vue
+++ src/App.vue
@@ -9,80 +9,80 @@ function newTask() {
 </script>
 
 <template>
-  <header>
-    <h1>Track Your Tasks</h1>
-  </header>
+    <header>
+        <h1>Track Your Tasks</h1>
+    </header>
 
-  <div class="wrapper">
-    <aside>
-      <button @click="newTask">+ Add Task</button>
-    </aside>
+    <div class="wrapper">
+        <aside>
+            <button @click="newTask">+ Add Task</button>
+        </aside>
 
-    <main>
-      <RouterView />
-    </main>
-  </div>
+        <main>
+            <RouterView />
+        </main>
+    </div>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+    line-height: 1.5;
+    max-height: 100vh;
 }
 
 header h1 {
-  text-align: center;
+    text-align: center;
 }
 
 .wrapper {
-  display: flex;
-  flex-wrap: 1;
-  gap: 2em;
+    display: flex;
+    flex-wrap: 1;
+    gap: 2em;
 }
 
 aside {
-  flex-basis: 20em;
-  flex-grow: 1;
-  height: 100vw;
-  padding-top: 1em;
-  padding-left: 1em;
-  background-color: #f5f5f5;
+    flex-basis: 20em;
+    flex-grow: 1;
+    height: 100vw;
+    padding-top: 1em;
+    padding-left: 1em;
+    background-color: #f5f5f5;
 }
 
 main {
-  flex-basis: 0;
-  flex-grow: 999;
+    flex-basis: 0;
+    flex-grow: 999;
 }
 </style>
 
 <style>
-  button {
+button {
     padding: 0.5em 1em;
     border: 1px solid dimgrey;
     border-radius: 10px;
-  }
+}
 
-  .close-btn {
+.close-btn {
     position: absolute;
     top: 0;
     right: 0.5em;
-  }
+}
 
-  .task-view {
+.task-view {
     height: 100vw;
     padding-top: 5em;
     background-color: #cfd8dc;
-  }
+}
 
-  .task-view > .task {
+.task-view > .task {
     margin: auto;
-  }
+}
 
-  .task {
+.task {
     position: relative;
     border-radius: 1em;
     padding: 2em;
     width: 50em;
     background-color: white;
-  }
+}
 </style>
